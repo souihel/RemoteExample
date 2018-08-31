f0a013b98c1f0da347e2eff2ea946d8f2b764965
dodo <souiheld@yahoo.com>
2018-08-31 18:13:17 +0100

    intial comment
    Signed-off-by: dodo <souiheld@yahoo.com>

---
+++ first/firstTwo.h
@@ -0,0 +1,13 @@
+/*
+ * firstTwo.h
+ *
+ *  Created on: 31þ/08þ/2018
+ *      Author: dodo
+ */
+
+#ifndef FIRSTTWO_H_
+#define FIRSTTWO_H_
+
+
+
+#endif /* FIRSTTWO_H_ */
---
+++ first/firstone.c
@@ -0,0 +1,13 @@
+/*
+ * firstone.c
+ *
+ *  Created on: 31þ/08þ/2018
+ *      Author: dodo
+ */
+
+#include(stdio.h)
+int main(void) {
+	printf("Hello world!\n");
+	printf("hello world again\n");
+	return 0;
+}
